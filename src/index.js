38f43b4f92f2283ad194d087bda35f6ade917cd0
Ekweaga <hikay133@gmail.com>
2022-06-06 02:04:02 +0100

    React-redux store management--- in progress

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { store } from './Redux/store';
+import { store,persistor } from './Redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <PersistGate persistor={store}>
+    <PersistGate persistor={persistor}>
        <App />
     </PersistGate>
   <React.StrictMode>
--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect'
 import Footer from '../../components/common/Footer'
 import Logo from '../../components/common/Logo'
 import Menu from '../../components/common/Menu'
 import EmptyCart from '../../components/Emptycart'
+import { selectCartItems, selectCartItemsCount, selectCartTotal } from '../../Redux/Cartfolder/cartselector'
 
-function Cart() {
+
+function Cart({cartCount, cartList,cartTotal}) {
+  console.log(cartCount, cartList,cartTotal);
   return (
     <>
    
@@ -13,7 +18,7 @@ function Cart() {
 
     </div>
     <EmptyCart/>
-    {/*<div className='orders'>
+    <div className='orders'>
 <div style={{borderBottom:'2px solid red',marginTop:'50px',marginBottom:'50px',width:'100%',maxWidth:'600px'}}>
   <h1>Your Cart</h1>
 </div>
@@ -21,7 +26,7 @@ function Cart() {
   <Menu/>
 </div>
     </div>
-  <div style={{borderTop:'2px solid red',marginTop:'50px',marginBottom:'50px',textAlign:'right'}}><h1 style={{textAlign:'right'}}>Total : #450</h1></div>*/}
+  <div style={{borderTop:'2px solid red',marginTop:'50px',marginBottom:'50px',textAlign:'right'}}><h1 style={{textAlign:'right'}}>Total : #450</h1></div>
 
     <div>
       <Footer />
@@ -29,5 +34,11 @@ function Cart() {
     </>
   )
 }
+const  mapStateToProps = createStructuredSelector({
+  cartCount:selectCartItemsCount ,
+  cartList:selectCartItems,
+  cartTotal : selectCartTotal
 
-export default Cart
+})
+
+export default connect(mapStateToProps)(Cart)
